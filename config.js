0e366550894c49154fbe1d8b74fca3f9ae67e55b
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-07-22 12:45:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.owner = [
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = ['62882009085754', '6285784791132'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
