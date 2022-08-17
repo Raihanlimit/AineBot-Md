f01aae460144806959002ecb745d12259240efc5
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-08-17 17:06:07 +0700

    Update and rename clear.js to clearchat.js

---
+++ plugins/clearchat.js
@@ -0,0 +1,17 @@
+async function handler(m, { conn }) {
+try {
+conn.chatModify({
+delete: true,
+lastMessages: [{key: m.key, messageTimestamp: m.messageTimestamp}]
+},
+m.chat)
+m.reply("chat ini telah dihapus pada whatsapp bot")
+} catch {
+m.reply("hapus chat gagal :(")
+}
+
+}
+handler.help = ['clearchat'],
+handler.tags = ['group'],
+handler.command = /^clearchat$/i
+export default handler
