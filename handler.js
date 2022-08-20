a52f2d3f533553a119bca3cc49b0988d44fbabbe
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-08-20 23:31:34 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -929,7 +929,7 @@ Untuk mematikan fitur ini, ketik
         conn.delay(10000) // supaya tidak spam
     })*/
 
-  conn.ws.on('CB:call', async (json) => {
+conn.ws.on('CB:call', async (json) => {
     console.log(json.content)
     const callerId = json.content[0].attrs['call-creator']
     if (json.content[0].tag == 'offer') {
