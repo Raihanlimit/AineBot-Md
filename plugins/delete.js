8d32f41fc2166988270478268ecfdb537b03a2bb
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-08-22 13:57:37 +0700

    Update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -1,22 +1,21 @@
 let handler = function (m) {
-    /*if (!m.quoted) throw false
-    let { chat, fromMe, isBaileys } = m.quoted
-    if (!fromMe) throw false
-    if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
-    conn.sendMessage(chat, { delete: m.quoted.vM.key })*/
-    if (!m.quoted) throw false
-    let { chat, fromMe, id, isBaileys } = m.quoted
-    if (!isBaileys) throw 'Reply pesan yang akan dihapus!'
-    conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.quoted.id, participant: m.quoted.sender } })
-
+  //if (!m.quoted) throw false
+ //let { chat, fromMe, id, isBaileys } = m.quoted
+ // if (!isBaileys) throw 'Reply pesan yang ingin dihapus!'
+ let key = {}
+ try {
+ 	key.remoteJid = m.quoted ? m.quoted.fakeObj.key.remoteJid : m.key.remoteJid
+	key.fromMe = m.quoted ? m.quoted.fakeObj.key.fromMe : m.key.fromMe
+	key.id = m.quoted ? m.quoted.fakeObj.key.id : m.key.id
+ 	key.participant = m.quoted ? m.quoted.fakeObj.participant : m.key.participant
+ } catch (e) {
+ 	console.error(e)
+ }
+ conn.sendMessage(m.chat, { delete: key })
 }
-handler.help = ['del', 'delete']
-handler.tags = ['tools']
-
-handler.command = /^(deladm)?$/i
+handler.help = ['delete']
+handler.tags = ['info']
+handler.command = /^(del|delete|unsend?)$/i
 handler.limit = false
 handler.admin = true
-handler.group = true
-handler.botAdmin = true
-
 module.exports = handler
