a54868254b9b32ee6b3a57034dff540001aa599d
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-08-22 16:36:28 +0700

    Fix(?)

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,27 +1,14 @@
-let limit = 50
-// const { servers, yta } = require('../lib/y2mate')
-const { youtubedl, youtubedlv2 } = require('@bochilteam/scraper')
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+let limit = 30
+let fetch = require('node-fetch')
+const { servers, yta } = require('../lib/y2mate')
+let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
+  if (!args || !args[0]) throw `contoh:\n${usedPrefix + command} https://www.youtube.com/watch?v=yxDdj_G9uRY`
   let chat = global.db.data.chats[m.chat]
-  // let server = (args[1] || servers[0]).toLowerCase()
-  const isY = /y(es)/gi.test(args[1])
-  const { thumbnail, audio: _audio, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0]))
-  let audio, link = '', lastError
-  for (let i in _audio) {
-    try {
-      audio = _audio[i]
-      link = await audio.download()
-      if (link) break
-    } catch (e) {
-      lastError = e
-      continue
-    }
-  }
-  if (!link) throw lastError
-  // let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < audio.fileSize
-  if (!isY) conn.sendFile(m.chat, dl_link, title + '.mp3', `
+  let server = (args[1] || servers[0]).toLowerCase()
+  let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
+  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
+  m.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, download sendiri: ${dl_link}` : global.wait)
+  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
 ┏┉━━━━━━━━━━━❏
 ┆ *YOUTUBE MP3*
 ├┈┈┈┈┈┈┈┈┈┈┈
@@ -42,9 +29,20 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
      }
   })
 }
-handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url> <without message>`)
+handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url>`)
 handler.tags = ['downloader']
 handler.command = /^yt(a|mp3)$/i
-handler.limit = true
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
 handler.exp = 0
+handler.limit = true
+
 module.exports = handler
