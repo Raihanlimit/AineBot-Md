6b5c9e3988db5f80a11227d094777568bad8cfa0
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-08-22 14:11:39 +0700

    Update and rename delete.js to deladm.js

--- plugins/deladm.js
+++ plugins/deladm.js
@@ -15,7 +15,7 @@ let handler = function (m) {
 }
 handler.help = ['delete']
 handler.tags = ['info']
-handler.command = /^(del|delete|unsend?)$/i
+handler.command = /^(deladm)$/i
 handler.limit = false
 handler.admin = true
 module.exports = handler
