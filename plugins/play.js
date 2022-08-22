6fd8a0f53646e5d5de30cb44c8e28eabffe05df2
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-08-22 09:51:43 +0700

    Anuu

--- plugins/play.js
+++ plugins/play.js
@@ -1,63 +1,28 @@
-let limit = 30
-let yts = require('yt-search')
-let fetch = require('node-fetch')
-const { servers, yta, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, command, usedPrefix, text, isPrems, isOwner }) => {
-  if (!text) throw `What song are you want find?\n*Example:* ${usedPrefix}play alan walker faded`
-  let chat = global.db.data.chats[m.chat]
-  let results = await yts(text)
-  let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Video/Audio Not Found'
-  let isVideo = /2$/.test(command)
-  let yt = false
-  let usedServer = servers[0]
-  for (let i in servers) {
-    let server = servers[i]
-    try {
-      yt = await (isVideo ? ytv : yta)(vid.url, server)
-      usedServer = server
-      break
-    } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\ntry another server...'}`)
-    }
-  }
-  if (yt === false) throw 'All servers can\'t 😕'
-  let { dl_link, thumb, title, filesize, filesizeF } = yt
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*${isLimit ? 'Pakai ': ''}Link:* ${await shortlink(dl_link)}
-*Server y2mate:* ${usedServer}
-
-_*Please wait while processing..*_
-`.trim(), m)
-let _thumb = {}
-try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
-catch (e) { }
-if (!isLimit) conn.sendFile(m.chat, dl_link, '${title}.mp3', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*Server y2mate:* ${usedServer}
-`.trim(), m, false,  {
-  ..._thumb,
-  asDocument: chat.useDocument
-})
+import { youtubeSearch } from '@bochilteam/scraper'
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
+  let vid = (await youtubeSearch(text)).video[0]
+  if (!vid) throw 'Video/Audio Tidak ditemukan'
+  let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
+  const url = 'https://www.youtube.com/watch?v=' + videoId
+  await conn.sendHydrated(m.chat, `
+📌 *Title:* ${title}
+🔗 *Url:* ${url}
+🖹 *Description:* ${description}
+⏲️ *Published:* ${publishedTime}
+⌚ *Duration:* ${durationH}
+👁️ *Views:* ${viewH}
+  `.trim(), author, thumbnail + '.png', url, '📺Go To Youtube!', null, null, [
+    ['Audio 🎧', `${usedPrefix}yta ${url} yes`],
+    ['Video 🎥', `${usedPrefix}ytv ${url} yes`],
+    ['Youtube Search🔎', `${usedPrefix}yts ${url}`]
+  ], m)
 }
-handler.help = ['play', 'play2'].map(v => v + ' <search>')
+handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
 handler.command = /^play2?$/i
-handler.premium = false
-handler.group = false
 
 handler.exp = 0
 handler.limit = true
 
 module.exports = handler
-
-async function shortlink(url) {
-isurl = /https?:\/\//.test(url)
-return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
-}
