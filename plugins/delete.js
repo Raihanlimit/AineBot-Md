08ab5bbd5482fdb1597fab24434c83c697f6756a
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-08-22 13:49:10 +0700

    Update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -6,14 +6,16 @@ let handler = function (m) {
     conn.sendMessage(chat, { delete: m.quoted.vM.key })*/
     if (!m.quoted) throw false
     let { chat, fromMe, id, isBaileys } = m.quoted
-    if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
-    conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: true, id: m.quoted.id, participant: m.quoted.sender } })
+    if (!isBaileys) throw 'Reply pesan yang akan dihapus!'
+    conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.quoted.id, participant: m.quoted.sender } })
 
 }
 handler.help = ['del', 'delete']
 handler.tags = ['tools']
 
 handler.command = /^del(ete)?$/i
-handler.limit = true
+handler.limit = false
+handler.admin = true
+handler.group = true
 
 module.exports = handler
