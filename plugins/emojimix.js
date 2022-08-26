921ef42005847880a710c91ffcbf76bfe3ec9655
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-08-26 18:14:51 +0700

    Fix typo

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -6,7 +6,7 @@ const { sticker } = require('../lib/sticker')
 let handler = async (m, { conn, text, args }) => {
 if (!args[0]) throw 'Contoh penggunaan:\n\n*.emojimix 🤨+😣*'
    	let [emoji1, emoji2] = text.split`&`
-		let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
+		let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparant&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
 		for (let res of anu.results) {
 			let stiker = await sticker(false, res.url, packname, author)
 			conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
