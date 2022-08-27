158540df6d13b87d03d4a8906b3512de8adabdfb
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-08-27 07:44:08 +0700

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,14 +1,15 @@
-import instagramGetUrl from "instagram-url-direct";
+const instagramGetUrl = require("instagram-url-direct")
 let handler = async (m, { conn, args, command, usedPrefix }) => {
   if (!args[0])
-    throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`;
-  let response = await instagramGetUrl(args[0]);
+    throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
+  let response = await instagramGetUrl(args[0])
   for (let data of response.url_list) {
-    await conn.sendFile(m.chat, data, "ig.mp4", data, m);
+    await conn.sendFile(m.chat, data, "ig.mp4", data, m)
   }
-};
-handler.help = ["instagram <url>"];
-handler.tags = ["downloader"];
-
+}
+handler.help = ["instagram <url>"]
+handler.tags = ["downloader"]
 handler.command = /^i(nsta)?g(ram)?(dl)?$/i
+handler.limit = true
+handler.group = false
 module.exports = handler
