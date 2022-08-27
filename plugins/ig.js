14b606d47242f2394d196ab1c4036d600105a624
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-08-27 07:26:28 +0700

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,21 +1,14 @@
-let { instagramdlv3, instagramdlv4 } = require('@bochilteam/scraper')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+import instagramGetUrl from "instagram-url-direct";
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+  if (!args[0])
+    throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`;
+  let response = await instagramGetUrl(args[0]);
+  for (let data of response.url_list) {
+    await conn.sendFile(m.chat, data, "ig.mp4", data, m);
+  }
+};
+handler.help = ["instagram <url>"];
+handler.tags = ["downloader"];
 
-    if (!args[0]) throw `*Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_medium=copy_link`
-    if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `*Link salah! Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_medium=copy_link`
-    const results = await instagramdlv3(args[0]).catch(async _ => await instagramdlv4(args[0]))
-    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `🔗 *Url:* ${await shortlink(url)}\n*${global.wm}*`, m)
-}
-
-handler.help = ['ig'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = /^(ig|igdl|instagram)$/i
-handler.limit = true
-handler.group = false
-
+handler.command = /^i(nsta)?g(ram)?(dl)?$/i
 module.exports = handler
-
-async function shortlink(url) {
-isurl = /https?:\/\//.test(url)
-return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
-}
