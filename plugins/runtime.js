9ca7b7b24ee8d2caaf10cf9b06f3fdf089a30809
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-08-29 08:39:02 +0700

    Update runtime.js

--- plugins/runtime.js
+++ plugins/runtime.js
@@ -11,9 +11,8 @@ handler.command = /^(up|run)time$/i
 module.exports = handler
 
 function clockString(ms) {
-  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+	let h = isNaN(ms) ? '--' : Math.floor(ms % (3600 * 24) / 3600)
+	let m = isNaN(ms) ? '--' : Math.floor(ms % 3600 / 60)
+	let s = isNaN(ms) ? '--' : Math.floor(ms % 60)
+        return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
