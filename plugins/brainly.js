e8e80fdd7f3eb44177460405f2623b15bd1e25ce
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-08-29 08:54:15 +0700

    Update brainly.js

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -1,30 +1,29 @@
 let { Brainly } = require('brainly-scraper-v2')
 const brainly = new Brainly('id')
 
-let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `Example use ${usedPrefix}${command} apa itu javascript`
-  let res = await brainly.search(text, "id")
-  let answer = res
-    .map(
-      ({ question, answers }, i) => `
-_*PERTANYAAN KE ${i + 1}*_
-${formatTags(question.content)}${question.closed ? "(terjawab)" : ""}\n${
-        "```" + question.education
-      }•${question.grade + "```"}\n\n${answers.map(
-        (v, i) => `
-*JAWABAN KE ${i + 1}*${v.confirmed ? " *(Verified)*" : ""}${
-          v.isBest ? " (Terpilih)" : ""
-        }
-${formatTags(v.content)}
-Terima Kasih ${v.thanksCount} (${v.ratesCount} pilih)\n`
-      ).join``}`
-    )
-    .join(`\n\n\n`);
-  m.reply(answer);
-};
-handler.help = ["brainly <soal>"]
-handler.tags = ["internet"]
-handler.command = /^brainly$/i
+let handler = async (m, { conn, text }) => {
+	if (!text) throw 'Input Query'
+	let res = await brainly.search(text, 'id').catch(() => null)
+	console.log(res)
+	if (res) {
+		let answer = res.map(({ question, answers }, i) => `
+*Pertanyaan*${question.grade ? ` (${question.grade})` : ''}\n${question.content}${answers.map((v, i) => `
+*Jawaban Ke ${i + 1}*${v.verification ? ' (Verified)' : ''}${v.isBest ? ' (Best)' : ''}
+${v.content}${v.attachments.length > 0 ? `\n*Media Url*: ${v.attachments.join(', ')}` : ''}`).join``}`).join('\n' + '-'.repeat(45))
+		m.reply(answer.trim())
+	} else {
+		let answer = await (await fetch(API('violetics', '/api/media/brainly', { query: text }, 'apikey'))).json()
+		answer = answer.result
+		if (!answer.length) throw 'Not found'
+		for (let x = 0; x < answer.length; x++) {
+			await m.reply(`*${answer[x].pertanyaan}*\n_${answer[x].source}_\n${answer[x].jawaban}`)
+			await baileys.delay(2000)
+		}
+	}
+}
+handler.help = handler.alias = ['brainly']
+handler.tags = ['internet']
+handler.command = /^(brainly)$/i
 
 handler.limit = true
 
