25a652fd8694e354807d48369ec82d62ae19967a
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-04 17:59:49 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,16 +1,20 @@
-function handler(m) {
-  
-  const kontak = {
-	"displayName": 'My owner',
-	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('62882009085754@s.whatsapp.net')}\nitem1.TEL;waid=62882009085754:62882009085754\nitem1.X-ABLabel:Hanya Seorang Manusia Biasa.\nURL;My Github: https://github.com/Raihanlimit\nEMAIL;Email Owner: recell155@gmail.com\nORG: NOT A BOT + NO SAVE\nTEL;My Number Bot;waid=19378825461:19378825461\nEND:VCARD`
+const delay = time => new Promise(res => setTimeout(res, time))
+let handler = async(m, { conn }) => {
+	conn.p = conn.p ? conn.p : {}
+	let id = m.chat
+	ftroli = { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: 9999999999999999999999999999999999999999999999999999999, status: 1, surface: 1, message: wm, orderTitle: wm, sellerJid: '0@s.whatsapp.net' } } }
+	fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': require('fs').readFileSync('./src/logo.jpg'), thumbnail: require('fs').readFileSync('./src/logo.jpg'),sendEphemeral: true}}}
+	conn.p[id] = [
+	await conn.sendKontak(m.chat, kontak2, fkontak, { contexInfo: { forwardingScore: 99999, isForwarded: true } })
+	]
+	await delay(100)
+  return conn.sendMessage(m.chat, { text: `Hay kak @${await m.sender.split('@')[0]}, itu nomor ownerku jangan dispam yah bang`, mentions: [m.sender] }, { quoted: conn.p[id][0] })
+  await delay(100)
+  return delete conn.p[id]
 }
 
-conn.sendMessage(m.chat, { contacts: { contacts: [kontak] }}, { quoted: m })
-  
-}
-handler.help = ['owner', 'creator']
+handler.help = ['owner']
 handler.tags = ['info']
-
 handler.command = /^(owner|creator)$/i
 
 module.exports = handler
