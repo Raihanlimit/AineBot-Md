19eea551f2bbc706c05881cfe14bbb4e728cba77
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-04 18:19:05 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,12 +1,6 @@
 function handler(m) {
-  
-  const kontak = {
-	"displayName": 'My owner',
-	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('62882009085754@s.whatsapp.net')}\nitem1.TEL;waid=62882009085754:62882009085754\nitem1.X-ABLabel:Hanya Seorang Manusia Biasa\nURL;My Github: https://github.com/Raihanlimit\nEMAIL;Email Owner: recell155@gmail.com\nORG: NOT A BOT + NO SAVE\nTEL;My Number Bot;waid=19378825461:19378825461\nEND:VCARD`
-}
-
-conn.sendMessage(m.chat, { contacts: { contacts: [kontak] }}, { quoted: m })
-  
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
