2e2b4bef0eda2c6dfa2f9f351123af455c877a68
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-06 17:58:09 +0700

    Update facebook.js

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -1,17 +1,14 @@
-let { facebookDl } = require('./scraper.js')
-let { savefrom } = require('@bochilteam/scraper')
-
-let handler = async (m, { conn, args }) => {
-	if (!args[0]) throw 'Input URL'
-	let res = await facebookDl(args[0]).catch(async _ => await savefrom(args[0])).catch(_ => null)
-	if (!res) throw 'Can\'t download the post'
-	let url = res?.url?.[0]?.url || res?.url?.[1]?.url || res?.['720p'] || res?.['360p']
-	await m.reply('_Process Sir...._')
-	conn.sendMessage(m.chat, { video: { url }, caption: res?.meta?.title || '' }, { quoted: m })
+const { facebookdl, facebookdlv2 } = require('@bochilteam/scraper')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://fb.watch/azFEBmFRcy/`
+    const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
+    for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `🔗 *Url:* ${url}`, m)
 }
-handler.help = ['facebook']
+handler.help = ['facebook'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.alias = ['fb', 'fbdl', 'facebook', 'facebookdl']
-handler.command = /^((facebook|fb)(dl)?)$/i
+
+handler.command = /^((facebook|fb)(downloder|dl)?)$/i
+handler.limit = true
+handler.group = false
 
 module.exports = handler
