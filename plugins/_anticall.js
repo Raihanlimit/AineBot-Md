9573e13f16103e312e93167cd72dd80cdce037ba
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-06 22:22:53 +0700

    Update _anticall.js

--- plugins/_anticall.js
+++ plugins/_anticall.js
@@ -7,7 +7,7 @@ const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function (
     resolve()
 }, ms))
 
-export async function all(m) {
+handler.all = async function(m) {
 	if (m.fromMe && m.isBaileys) return !0
 	let text
 	let setting = global.db.data.settings[this.user.jid]
