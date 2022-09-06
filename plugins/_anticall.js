e7d9854e97de498628f3093daad29cab8768a49c
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-06 22:20:53 +0700

    Update _anticall.js

--- plugins/_anticall.js
+++ plugins/_anticall.js
@@ -1,5 +1,5 @@
-const { WAMessageStubType } = (await import('@adiwajshing/baileys')).default
-import { format } from 'util';
+const { WAMessageStubType } = require('@adiwajshing/baileys')
+const { format } = require('util')
 
 const isNumber = x => typeof x === 'number' && !isNaN(x)
 const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function () {
@@ -9,7 +9,7 @@ const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function (
 
 export async function all(m) {
 	if (m.fromMe && m.isBaileys) return !0
-	let text;
+	let text
 	let setting = global.db.data.settings[this.user.jid]
 	if(!setting.anticall) return 
 	
