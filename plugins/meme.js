e57f7350a3d0c80cad9b2f6dfce2c0fc755bf6d8
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-06 22:59:53 +0700

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn }) => {
 }
 
 handler.help = ['meme']
-handler.tags = ['random']
+handler.tags = ['Random']
 handler.register = false
 handler.command = /^(meme)$/i
 handler.limit = true
