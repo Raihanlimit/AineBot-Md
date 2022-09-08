b1601afaeda5198e58d152d8c75f2b188dc35524
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-08 10:32:40 +0700

    Update anime-manga.js

--- plugins/anime-manga.js
+++ plugins/anime-manga.js
@@ -4,9 +4,9 @@ let handler = async(m, { conn, text }) => {
   let res = await fetch(global.API('https://api.jikan.moe', '/v3/search/manga', { q: text }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  let { title, synopsis, chapters, url, volumes, score, image_url } = json.results[0]
+  let { title, synopsis, chapter, url, volumes, score, image_url } = json.results[0]
 let mangaingfo = `*Title:* ${title}
-*Chapters:* ${chapters}
+*Chapter:* ${chapter}
 *Volumes:* ${volumes}
 *Score:* ${score}
 *Synopsis:* ${synopsis}
