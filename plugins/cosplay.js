794240decc8daf071ee32aab2481718db6a71230
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-08 10:07:34 +0700

    Update cosplay.js

--- plugins/cosplay.js
+++ plugins/cosplay.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, command }) => {
 	conn.sendButton(m.chat, 'Watashiwa Anime Desu (≧ω≦)', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
 }
 handler.command = /^(cosplay)$/i
-handler.tags = ['internet']
+handler.tags = ['anime']
 handler.help = ['cosplay']
 handler.premium = false
 handler.limit = true
