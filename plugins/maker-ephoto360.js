fad1eb9114cb0c2e2b036aeb4cb63be5681c6ce6
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-08 19:29:58 +0700

    Update maker-ephoto360.js

--- plugins/maker-ephoto360.js
+++ plugins/maker-ephoto360.js
@@ -5,7 +5,7 @@ Contoh : ${_p}${command} ${pickRandom(['Ziv San','Fory'])}`
      let ziv = `https://api.violetics.pw/api/ephoto360/${command}?apikey=aa37-2814-15e2&text=${text}`
      conn.sendButton(m.chat, 'https://telegra.ph/file/28f709a2a84758d9c9286.jpg', `Tunggu Sebentar Lagi Di Proses...\n\nKalo Error/Bug Silakan Chat 
 @${global.owner[1]}`, wm, 'Owner', '.owner', m)
-     conn.sendButton(m.chat, ren, 'Nih', wm3, 'Thanks', 'thanks', m, {
+     conn.sendButton(m.chat, ziv, 'Nih', wm3, 'Thanks', 'thanks', m, {
     quoted: m,
     contextInfo: { forwardingScore: 99999, isForwarded: true,
         externalAdReply: {
