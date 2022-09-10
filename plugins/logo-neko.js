d1ae2d0ae20a279311805e40db4acc6bd15fc02b
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-10 16:36:58 +0700

    Create logo-neko.js

---
+++ plugins/logo-neko.js
@@ -0,0 +1,14 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   let response = args.join(' ').split('|')
+  if (!args[0]) throw 'Masukkan Textnya. Contoh: Ren|Bot'
+  m.reply('Proses...')
+  let res = `https://ziy.herokuapp.com/api/maker/girlneko?text1=${response[0]}&text2=${response[1]}&apikey=xZiyy`
+  conn.sendFile(m.chat, res, 'neko.jpg', `Sudah Jadi`, m, false)
+}
+handler.help = ['logoneko'].map(v => v + ' <text|text>')
+handler.tags = ['maker']
+handler.command = /^(logoneko)$/i
+handler.limit = true
+
+module.exports = handler
