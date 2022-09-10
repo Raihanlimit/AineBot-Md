59e8d7ee6a6978ee4983404c0dab2e3e5d563ede
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-10 16:27:28 +0700

    Update logo-rem.js

--- plugins/logo-rem.js
+++ plugins/logo-rem.js
@@ -1,12 +1,12 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 let response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Text, Contoh Ren|Bot'
+  if (!args[0]) throw 'Masukkan Textnya. Contoh Ren'
   m.reply('proses..')
   let res = `https://ziy.herokuapp.com/api/maker/rem?nama=${response[0]}&apikey=xZiyy`
   conn.sendFile(m.chat, res, 'rem.jpg', `Nih Bang`, m, false)
 }
-handler.help = ['logorem'].map(v => v + ' <text|text>')
+handler.help = ['logorem'].map(v => v + ' <text>')
 handler.tags = ['maker']
 handler.command = /^(logorem)$/i
 handler.limit = true
