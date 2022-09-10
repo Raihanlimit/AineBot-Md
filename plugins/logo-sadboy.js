91108cff0f59bbf199c7911ec30c4ca6aa3a2c44
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-10 16:28:18 +0700

    Update logo-sadboy.js

--- plugins/logo-sadboy.js
+++ plugins/logo-sadboy.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 let response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Parameter'
+  if (!args[0]) throw 'Masukkan Textnya. Contoh: Ren'
   m.reply('proses..')
   let res = `https://ziy.herokuapp.com/api/maker/sadboy?text1=${response[0]}&text2=${response[1]}&apikey=xZiyy`
   conn.sendFile(m.chat, res, 'gfx3.jpg', `Nih kak`, m, false)
