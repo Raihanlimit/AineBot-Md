340ebb980d3155106f64346038c746e6b57e3d82
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-10 16:30:57 +0700

    Update logo-kaneki.js

--- plugins/logo-kaneki.js
+++ plugins/logo-kaneki.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    let response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Parameter'
+  if (!args[0]) throw 'Masukkan Textnya. Contoh: Ren'
   m.reply('Proses...')
   let res = `https://ziy.herokuapp.com/api/maker/kaneki?nama=${response[0]}&apikey=xZiyy`
   conn.sendFile(m.chat, res, 'kaneki.jpg', `Done Bang`, m, false)
