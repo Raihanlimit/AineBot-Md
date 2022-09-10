be4664049bd412f9cfed1a347855fc5de9f7572d
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-10 16:39:50 +0700

    Update hornycard.js

--- plugins/hornycard.js
+++ plugins/hornycard.js
@@ -1,14 +1,10 @@
 let handler = async (m, { conn }) => {
-  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/horny', {
-    avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'hornycard.png', 'horny:>', m)
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/horny', {
+avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+}), 'hornycard.png', '*Horny:>*', m)
 }
-
 handler.help = ['hornycard', 'hornylicense']
-handler.tags = ['maker']
-handler.limit = true
-
-handler.command = /^(horny(card|license))$/i
-
+handler.tags = ['maker'] 
+handler.command = /^(horny(card|license))$/i 
 module.exports = handler
