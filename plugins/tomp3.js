9e5b64aa7c6632e622c8758014796854cf65e482
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-11 22:40:40 +0700

    Update tomp3.js

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -1,27 +1,18 @@
-const { toAudio, toPTT } = require('../lib/converter')
+let { toAudio } from '../lib/converter.js'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (/mp3|a(udio)?$/i.test(command)) {
-    if (!/video|audio/.test(mime)) throw `Balas video/audio dengan perintah *${usedPrefix + command}*`
+    let q = m.quoted ? m.quoted : m
+    let mime = (q || q.msg).mimetype || q.mediaType || ''
+    if (!/video|audio/.test(mime)) throw `Reply video/voice note you want to convert to audio/mp3 with command *${usedPrefix + command}*`
     let media = await q.download()
-    if (!media) throw 'Media tidak dapat diunduh'
+    if (!media) throw 'Can\'t download media'
     let audio = await toAudio(media, 'mp4')
-    if (!audio.data) throw 'Gagal melakukan konversi.'
-    await conn.sendFile(m.chat, audio.data, 'Audio Convert.mp3', '', m, 0, { mimetype: 'audio/mp4', asDocument: global.db.data.chats[m.chat].useDocument })
-  }
-  if (/vn|ptt$/i.test(command)) {
-    if (!/video|audio/.test(mime)) throw `Balas video/audio dengan perintah *${usedPrefix + command}*`
-    let media = await q.download()
-    if (!media) throw 'Media tidak dapat diunduh'
-    let audio = await toPTT(media, 'mp4')
-    if (!audio.data) throw 'Gagal melakukan konversi.'
-    await conn.sendFile(m.chat, audio.data, 'file.mp4', '', m, 1, { mimetype: 'audio/mp4' })
-  }
+    if (!audio.data) throw 'Can\'t convert media to audio'
+    conn.sendMessage(m.chat, { audio: audio.data,  mimetype: 'audio/mpeg' }, { quoted: m })
 }
-handler.help = ['tomp3', 'tovn']
+handler.help = ['tomp3']
 handler.tags = ['audio']
-handler.command = /^to(mp3|a(udio)?|vn|ptt)$/i
+handler.alias = ['tomp3', 'toaudio']
+handler.command = /^to(mp3|audio)$/i
 
 module.exports = handler
