e71301c8cc8bfe9cbfff9cf59358da2540566987
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-11 11:48:33 +0700

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,33 +1,53 @@
-let limit = 50
 let fetch = require('node-fetch')
-const { servers, ytv } = require('../lib/y2mate')
-let handler = async(m, { conn, args, isPrems, isOwner }) => {
-    if (!args || !args[0]) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
-    let chat = db.data.chats[m.chat]
-    let server = (args[1] || servers[0]).toLowerCase()
-    let { dl_link, thumb, title, filesize, filesizeF } = await ytv(args[0], servers.includes(server) ? server : servers[0])
-    let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-    m.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, download sendiri: ${dl_link}` : data.wait, m)
-    let _thumb = {}
-    try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } } catch (e) {}
-    m.reply(data.wait)
-    if (!isLimit) await conn.sendButtonVid(m.chat, dl_link, `*Title:* ${title}\n*Filesize:* ${filesizeF}`.trim(), wm, 'Audio', `.yta ${args}`, m, {
-  contextInfo: { mentionedJid: [m.sender],
-    externalAdReply :{
-    showAdAttribution: true,
-    mediaUrl: data.sc,
-    mediaType: 2,
-    description: data.deslink, 
-    title: titlink,
-    body: bodlink,
-    thumbnail: bg,
-    sourceUrl: data.sc
-     }}
-  })
+let { youtubedl, youtubedlv2, youtubedlv3 } = require('@bochilteam/scraper')
 
+let limit = 500
+let handler = async (m, { conn, args, isPrems, isOwner }) => {
+  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+  let chat = db.data.chats[m.chat]
+  const isY = /y(es)/gi.test(args[1])
+  const { thumbnail, video: _video, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
+  const limitedSize = (isPrems || isOwner ? 2000 : limit) * 1024
+  let video, source, res, link, lastError, isLimit
+  for (let i in _video) {
+    try {
+      video = _video[i]
+      if (isNaN(video.fileSize)) continue
+      isLimit = limitedSize < video.fileSize
+      if (isLimit) continue
+      link = await video.download()
+      if (link) res = await fetch(link)
+      isLimit = res?.headers.get('content-length') && parseInt(res.headers.get('content-length')) < limitedSize
+      if (isLimit) continue
+      if (res) source = await res.arrayBuffer()
+      if (source instanceof ArrayBuffer) break
+    } catch (e) {
+      video = source = link = null
+      lastError = e
+    }
+  }
+  if ((!(source instanceof ArrayBuffer) || !link || !res.ok) && !isLimit) throw 'Error: ' + (lastError || 'Can\'t download video')
+  if (!isY && !isLimit) await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
+*Title:* ${title}
+*🗎 Filesize:* ${video.fileSizeH}
+*${isLimit ? 'Pakai ' : ''}Link:* ${link}
+`.trim(), m)
+  let _thumb = {}
+  try { _thumb = { thumbnail: await (await fetch(thumbnail)).buffer() } }
+  catch (e) { }
+  if (!isLimit) await conn.sendFile(m.chat, link, title + '.mp4', `
+*Title:* ${title}
+*🗎 Filesize:* ${video.fileSizeH}
+`.trim(), m, false, {
+    ..._thumb,
+    asDocument: chat.useDocument
+  })
 }
-handler.help = ['ytmp4 <query>']
+handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url> <without message>`)
 handler.tags = ['downloader']
-handler.command = /^yt(v(idi?e?o)?|mp4)?$/i
+handler.command = /^yt(v|mp4)?$/i
+
+handler.exp = 0
 handler.limit = true
+
 module.exports = handler
