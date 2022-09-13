2f105ea6d25a14c04b9def56dd5da26c8419c894
Ren <89056745+Raihanlimit@users.noreply.github.com>
2022-09-13 09:57:36 +0700

    Update otakudesu.js

--- plugins/otakudesu.js
+++ plugins/otakudesu.js
@@ -22,7 +22,7 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 🧷 *BATCHSD:* ${result.batchSD}
 📎 *BATCHHD:* ${result.batchHD}
 `
-conn.sendButton(m.chat, datathumb, otaku, wm, 'menu', '.menu', m)
+conn.sendMessage(m.chat, datathumb, otaku, wm, 'menu', '.menu', m)
 }
 
 handler.help = ['otakudesu'].map(v => v + ' <Apa>')
